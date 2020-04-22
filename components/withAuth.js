f5415db95296623e3041371320f2bbb1df212999
abid <abzno1@gmail.com>
2020-04-22 09:53:59 +0100

    Import cleanup

--- components/withAuth.js
+++ components/withAuth.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Router from "next/router";
 import { isLoggedIn } from "../utils/authentication";
 import { isClientSide } from "../utils/environment";
